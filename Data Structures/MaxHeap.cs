977d17a3d6ac7acab6b2e27d748b934eb73e5b8d
André Pinheiro <andrepdpinheiro@gmail.com>
2017-08-05 23:19:30 +0100

    Added Heap sort algorithm

--- Data Structures/MaxHeap.cs
+++ Data Structures/MaxHeap.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp3
         public void Insert(T o)
         {
             items.Add(o);
-
             int elemIndex = items.Count - 1;
             int parentIndex = (elemIndex + 1) / 2 - 1;
 
@@ -24,7 +23,6 @@ namespace ConsoleApp3
                 T tmp = items[parentIndex];
                 items[parentIndex] = items[elemIndex];
                 items[elemIndex] = tmp;
-
                 elemIndex = parentIndex;
             }
         }
@@ -34,20 +32,20 @@ namespace ConsoleApp3
             T max = items[0];
             items[0] = items[items.Count - 1];
             items.RemoveAt(items.Count - 1);
-            MaxHeapify(0);
+            MaxHeapify(0, items.Count - 1);
             return max;
         }
 
-        public void MaxHeapify(int i)
+        public void MaxHeapify(int i, int n)
         {
             int leftIndex = 2 * (i + 1) - 1, rightIndex = 2 * (i + 1), largest;
 
-            if (leftIndex <= items.Count - 1 && items[leftIndex].CompareTo(items[i]) > 0)
+            if (leftIndex < n && items[leftIndex].CompareTo(items[i]) > 0)
                 largest = leftIndex;
             else
                 largest = i;
 
-            if (rightIndex <= items.Count - 1 && items[rightIndex].CompareTo(items[largest]) > 0)
+            if (rightIndex < n && items[rightIndex].CompareTo(items[largest]) > 0)
                 largest = rightIndex;
 
             if(largest != i)
@@ -55,7 +53,21 @@ namespace ConsoleApp3
                 T tmp = items[i];
                 items[i] = items[largest];
                 items[largest] = tmp;
-                MaxHeapify(largest);
+                MaxHeapify(largest, n);
+            }
+        }
+
+        public void Sort(int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                MaxHeapify(i, n);
+
+            for(int i = n - 1; i >= 0; i--)
+            {
+                T tmp = items[i];
+                items[i] = items[0];
+                items[0] = tmp;
+                MaxHeapify(0, i);
             }
         }
 
