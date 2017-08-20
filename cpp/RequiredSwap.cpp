4afd41b3d182bb392cdf7895532635d634b70876
André Pinheiro <andrepdpinheiro@gmail.com>
2017-08-20 13:52:57 +0100

    Added function to swap bytes of a given number

--- cpp/RequiredSwap.cpp
+++ cpp/RequiredSwap.cpp
@@ -9,7 +9,12 @@ int bitSwapCount(int a, int b)
     return count;
 }
 
+int swapBytes(int a) {
+    return static_cast<int>((((a & 0xAAAAAAAA) >> 1) != 0u) || (((a & 0x55555555) << 1) != 0));
+}
+
 int main() {
     cout << bitSwapCount(4, 500) << endl;
+    cout << swapBytes(5) << endl;
     return 0;
 }
